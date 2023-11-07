17dbfec33a6d6834fd756bfd24bf184970ed0f51
Vinicius Yoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2023-11-07 01:51:17 -0300

    Created UnitConversor

---
+++ __tests__/UnitConversor.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "@jest/globals"
+
+import { PostScript } from "../src/PostScript"
+import { UnitConversor } from "../src/UnitConversor"
+
+
+describe("UnitConversor", () => {
+    test("psToCm", () => {
+        const a4Size = PostScript.getPageSize("A4")
+        const a4MaxSize = Math.max(a4Size.width, a4Size.height)
+
+        const cmSize = UnitConversor.psToCm(a4MaxSize)
+        expect(cmSize).toBeCloseTo(29.7, 1)
+    })
+
+    test("cmToPs", () => {
+        const a4Size = PostScript.getPageSize("A4")
+        const a4MaxSize = Math.max(a4Size.width, a4Size.height)
+
+        const psSize = UnitConversor.cmToPs(29.7)
+        expect(psSize).toBeCloseTo(a4MaxSize, 1)
+    })
+})
---
+++ src/UnitConversor.ts
@@ -0,0 +1,32 @@
+import { PostScript } from "./PostScript"
+
+
+export class UnitConversor {
+
+
+    private static getA4MinSizeInPostScript(): number {
+        const a4Size = PostScript.getPageSize("A4")
+        return Math.min(a4Size.width, a4Size.height)
+    }
+
+    private static getA4MinSizeInCm(): number {
+        return 21
+    }
+
+
+    static psToCm(ps: number): number {
+        const psSize = this.getA4MinSizeInPostScript()
+        const cmSize = this.getA4MinSizeInCm()
+
+        const onePsInCm = cmSize / psSize
+        return onePsInCm * ps
+    }
+
+    static cmToPs(cm: number): number {
+        const psSize = this.getA4MinSizeInPostScript()
+        const cmSize = this.getA4MinSizeInCm()
+
+        const oneCmInPs = psSize / cmSize
+        return cm * oneCmInPs
+    }
+}
