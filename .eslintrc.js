251b3f057d50427e147de3a01054a81333f906c9
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-21 19:00:44 -0300

    chore: Update .eslintrc.js to new eslint config version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
+    root: true,
     extends: "@vinicius1313/eslint-config",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+    },
     ignorePatterns: [
         "assets",
         "dist",
